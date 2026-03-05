bed84b5e0074e46aa61bb1d0f27a1a49f46551e4
Mathias de Castro <mdcz@orange.fr>
2026-03-05 09:05:13 +0100

    refactoring mob programming 1

--- python/src/bank.py
+++ python/src/bank.py
@@ -1,4 +1,6 @@
 from typing import Dict
+
+from currency import Currency
 from .currency import Currency
 from .missing_exchange_rate_error import MissingExchangeRateError
 
@@ -10,16 +12,19 @@ class Bank:
         self._exchange_rate = exchange_rate
 
     @staticmethod
-    def create(currency1: Currency, currency2: Currency, rate: float) -> "Bank":
+    def createNewExchangeRate(fromCurrency: Currency, toCurrency: Currency, rate: float) -> "Bank":
         bank = Bank({})
-        bank.addEchangeRate(currency1, currency2, rate)
+        bank.addExchangeRate(fromCurrency, toCurrency, rate)
 
         return bank
     
-    def addEchangeRate(self, currency1: Currency, currency2: Currency, rate: float) -> None:
-        self._exchange_rate[f'{currency1.value}->{currency2.value}'] = rate
+    def addExchangeRate(self, fromCurrency: Currency, toCurrency: Currency, rate: float) -> None:
+        self._exchange_rate[f'{fromCurrency.value}->{toCurrency.value}'] = rate
 
-    def convert(self, amount: float, currency1: Currency, currency2: Currency) -> float:
-        if not (currency1.value == currency2.value or f'{currency1.value}->{currency2.value}' in self._exchange_rate):
-            raise MissingExchangeRateError(currency1, currency2)
-        return amount if currency1.value == currency2.value  else amount * self._exchange_rate[f'{currency1.value}->{currency2.value}']
+    def convert(self, amount: float, fromCurrency: Currency, toCurrency: Currency) -> float:
+        if not (self.canConvert(fromCurrency, toCurrency)):
+            raise MissingExchangeRateError(fromCurrency, toCurrency)
+        return amount if fromCurrency.value == toCurrency.value  else amount * self._exchange_rate[f'{fromCurrency.value}->{toCurrency.value}']
+
+    def canConvert(self, fromCurrency: Currency, toCurrency: Currency) -> bool:
+        return fromCurrency.value == toCurrency.value or f'{fromCurrency.value}->{toCurrency.value}' in self._exchange_rate

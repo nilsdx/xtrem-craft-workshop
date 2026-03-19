f79a48e0f036b447ac9f1790cb11f958964140f0
nilsdx <nils.durieux@gmail.com>
2026-03-19 15:49:55 +0100

    fix with_exchange_rate

--- python/src/xterm_craft_workshop/bank_builder.py
+++ python/src/xterm_craft_workshop/bank_builder.py
@@ -1,9 +1,11 @@
 from xterm_craft_workshop.bank import Bank
 from xterm_craft_workshop.currency import Currency
 
-
-
 class BankBuilder:
+    def __init__(self) -> None:
+        self.pivot_currency = Currency.EUR
+        self.exchange_rates = {}
+
     @staticmethod
     def a_bank() -> "BankBuilder":
         return BankBuilder()
@@ -13,7 +15,8 @@ class BankBuilder:
         return self
     
     def with_exchange_rate(self, currency: Currency, rate: float) -> "BankBuilder":
-        self.exchange_rates = {f"{self.pivot_currency.value}->{currency.value}": rate}
+        self.exchange_rates[f"{self.pivot_currency.value}->{currency.value}"] = rate
+        self.exchange_rates[f"{currency.value}->{self.pivot_currency.value}"] = 1/rate
         return self
     
     def build(self) -> Bank:
--- python/tests/test_portfolio.py
+++ python/tests/test_portfolio.py
@@ -5,12 +5,13 @@ from xterm_craft_workshop.currency import Currency
 from xterm_craft_workshop.missing_exchange_rate_error import MissingExchangeRateError
 from xterm_craft_workshop.money import Money
 from xterm_craft_workshop.portfolio import Portfolio
+from xterm_craft_workshop.bank_builder import BankBuilder
 
 
 class TestPortfolio:
     def test_empty_value_portfolio(self):
         portfolio = Portfolio()
-        bank = Bank({})
+        bank = BankBuilder.a_bank().with_pivot_currency(Currency.EUR).with_exchange_rate(Currency.USD, 1.2).build()
 
         eval = portfolio.evaluatePortfolio(Currency.EUR, bank)
 
@@ -18,8 +19,8 @@ class TestPortfolio:
 
     def test_portfolio_contains_10_eur(self):
         portfolio = Portfolio()
-        bank = Bank({})
-
+        bank = BankBuilder.a_bank().with_pivot_currency(Currency.EUR).with_exchange_rate(Currency.USD, 1.2).build()
+        
         portfolio.addMoney(Money(10, Currency.EUR))
         eval = portfolio.evaluatePortfolio(Currency.EUR, bank)
 
@@ -27,8 +28,7 @@ class TestPortfolio:
 
     def test_portfolio_contains_10_eur_when_evaluate_in_usd_with_exchange_rate(self):
         portfolio = Portfolio()
-        bank = Bank({})
-        bank.addExchangeRate(Currency.EUR, Currency.USD, 1.2)
+        bank = BankBuilder.a_bank().with_pivot_currency(Currency.EUR).with_exchange_rate(Currency.USD, 1.2).build()
 
         portfolio.addMoney(Money(10, Currency.EUR))
         eval = portfolio.evaluatePortfolio(Currency.USD, bank)
@@ -37,7 +37,7 @@ class TestPortfolio:
 
     def test_portfolio_contains_10_eur_when_evaluate_in_usd_without_exchange_rate(self):
         portfolio = Portfolio()
-        bank = Bank({})
+        bank = BankBuilder.a_bank().with_pivot_currency(Currency.EUR).with_exchange_rate(Currency.KRW, 1.2).build()
         portfolio.addMoney(Money(10, Currency.EUR))
 
         with pytest.raises(MissingExchangeRateError) as error:
@@ -47,8 +47,8 @@ class TestPortfolio:
 
     def test_portfolio_contains_10_eur_and_5_usd_when_evaluate_in_usd(self):
         portfolio = Portfolio()
-        bank = Bank.createNewExchangeRate(Currency.EUR, Currency.USD, 1.2)
-
+        bank = BankBuilder.a_bank().with_pivot_currency(Currency.EUR).with_exchange_rate(Currency.USD, 1.2).build()
+        
         portfolio.addMoney(Money(10, Currency.EUR))
         portfolio.addMoney(Money(5, Currency.USD))
 
